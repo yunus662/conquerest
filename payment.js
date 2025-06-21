c966ffce7841dca51136b83621cf6fbc56534b3c
yunus662 <imyaay234@gmail.com>
2025-06-21 11:55:04 -0700

    Update payment.js

--- payment.js
+++ payment.js
@@ -2,7 +2,7 @@
 
 export async function buyTickets(quantity = 1) {
   try {
-    const response = await fetch("https://your-replit-or-stripe-server.com/create-checkout-session", {
+    const response = await fetch("https://a56a19b3-9378-4691-b2ee-d49ea8516899-00-28bd8u7rlzv4s.spock.repl.co/__replco/workspace_iframe.html?initialPath=%2F&id=%3Arer%3A/create-checkout-session", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ quantity })
